bc47fc4290a57bee0f180b7db289a8e58359df48
Stark94 <ciotta.pa@hotmail.it>
2018-10-26 13:02:57 +0200

    FirstCommit

---
+++ secondaprova/src/main/java/it/paolo/ciotta/SecondaprovaApplication.java
@@ -0,0 +1,12 @@
+package it.paolo.ciotta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecondaprovaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecondaprovaApplication.class, args);
+	}
+}
